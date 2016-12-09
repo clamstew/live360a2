585c08fc32a810fdd1ef11cc926f377aaa5d5765
John Papa <john@johnpapa.net>
2016-12-09 14:38:23 -0500

    new lab is done. yippee

--- app/customers.component.ts
+++ app/customers.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
-  selector: 'my-customer',
-  templateUrl: 'customers.component.html'
+    moduleId: module.id,
+    selector: 'my-customer',
+    templateUrl: 'customers.component.html',
+    styleUrls: ['customers.component.css']
 })
 export class CustomerComponent {
-  message: string;
-  selectedCustomer = {id: 1, name: 'John', state: 'Florida' };
-  showMessage() {
-    this.message = `The customer name is ${this.selectedCustomer.name}`;
-  }
+    message: string;
+    customers = [
+      new Customer(1, 'John', 'Florida'),
+      new Customer(2, 'Ward', 'South Pole'),
+      new Customer(3, 'Jesse', 'Disney'),
+    ];
+    selectedCustomer: Customer; // = this.customers[0];
+
+    isSelected(c: Customer) {
+        return this.selectedCustomer === c;
+    }
+    showMessage() {
+        this.message = `The customer name is ${this.selectedCustomer.name}`;
+    }
+
+    selectIt(c: Customer) {
+        this.selectedCustomer = c;
+    }
+}
+
+class Customer {
+      constructor(public id: number, public name: string, public state: string){ }
 }
 
